f6da7aab16fffbffc9e0561cde34063630a001da
wjcheon <wonjoongcheon@gmail.com>
2017-04-06 10:13:01 +0900

    uploading version1.0

---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+## About this code  
+The script "main_dicom2hdr.py" is wirtten for reading dicom files and converting format to *.hdr , *.img  
+
+  
+  
+## Who am I 
+**Wonjoong Cheon**  
+Ph.D intergrated program  
+Medical Physics Lab. - SUMP Lab.  
+Samsung Advanced Institute for Health Science & Technology(SAIHST), SungKyunKwan University.  
+B.E. Dept. of Information and Communication Engineering , Yonsei University.  
+B.S. Dept. of Radiological Science, Yonsei University.  
+
+Laboratry 
+Samsung Medical Center (Medical Physics, 2014 ~ )  
+National Cancer Center (Computer Vision, 2013)  
+Vatech Vision reasearch Center (CT recon, 2012)  
+
+**Interest field**  
+Medical physics, Monte-carlo simulation, Machine learning  
+
+wonjoongcheon@gmail.com,   
+Samsung Medical Center (06351) 81 Irwon-ro Gangnam-gu, Seoul, Korea.  
--- main_dicom2hdr.py
+++ main_dicom2hdr.py
@@ -3,7 +3,9 @@
 Created on Wed Apr  5 12:23:59 2017
 
 @author: Wonjoong Jason Cheon
+@Date: 2017.04.06
 @Version 1.0 
+@
 """
 %reset
 #%%
@@ -13,7 +15,6 @@ import os
 import numpy
 from matplotlib import pyplot, cm 
 from matplotlib import pyplot as plt
-
 
 #%% File lodes
 os.chdir('D:\MEGA\Python\DICOM_PROJECT')
@@ -38,7 +39,7 @@ ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
 
 # Load spacing values (in mm)
 ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
-#%%
+#%% Generate coordinates for each axis
 
 x = numpy.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
 y = numpy.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
@@ -52,24 +53,27 @@ ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 for filenameDCM in lstFilesDCM:
     # read the file
     ds = dicom.read_file(filenameDCM)
-    # store the raw image data
+    # store the raw image data to target 3D matrix (i.e ArrayDicom)
     ArrayDicom[:, :, lstFilesDCM.index(filenameDCM)] = ds.pixel_array 
-    # Print
+    # Print progressing status
     if lstFilesDCM.index(filenameDCM) % 100 == 0:
         print ("{}/{} th file was sucessully loaded ".format(lstFilesDCM.index(filenameDCM), len(lstFilesDCM)))
                
-#%%
-pyplot.figure(dpi=300)
-pyplot.axes().set_aspect('equal', 'datalim')
-pyplot.set_cmap(pyplot.gray())
-pyplot.pcolormesh(x, y, numpy.flipud(ArrayDicom[:, :, 1]))
+#%%  Drawing 
+Dicom_selected_transverse = ArrayDicom[1:sz_ArrayDicom[0]+1,1:sz_ArrayDicom[1]+1,sz_ArrayDicom[2]/2]
+Dicom_selected_coronal = ArrayDicom[sz_ArrayDicom[0]/2,1:sz_ArrayDicom[1]+1,1:sz_ArrayDicom[2]+1]
+Dicom_selected_saggital = ArrayDicom[1:sz_ArrayDicom[0]+1,sz_ArrayDicom[1]/2,1:sz_ArrayDicom[2]+1]
 
-#%%
-Dicom_selected = ArrayDicom[1:671,1:671,255]
-plt.figure()
-plt.imshow(Dicom_selected)
+num_subplot = 3
+def show_slices(slices):
+    """ Function to display row of image slices """
+    fig, axes = plt.subplots(1, num_subplot)
+    for i, slice in enumerate(slices):
+        axes[i].imshow(slice.T, cmap="gray", origin="lower")
 
-#%%
+
+show_slices([Dicom_selected_transverse, Dicom_selected_coronal, Dicom_selected_saggital])
+plt.suptitle("Center slices for Dicom image")  
 
 
 #%%
@@ -89,7 +93,7 @@ translation_affine = np.array([[1, 0, 0, 0],
 #translation_affine = np.diag([1, 1, 1, 1])
     #%%
 
-img_dicom = nib.Nifti1Image(ArrayDicom.astype(np.int16), affine)
+img_dicom = nib.Nifti1Image(ArrayDicom.astype(np.int16), translation_affine)
 header_dicom = img_dicom.header
 print(header_dicom)
 # Should be change some variable in header_dicom 
